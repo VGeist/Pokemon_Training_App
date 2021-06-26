b9fd90fdfb6ec249bd096f3576d61ca2728fd3c8
Victor Geist <victor75664@cccneb.edu>
2021-06-26 10:27:03 -0500

    - Added frmPokemonForms

---
+++ Pokemon_Training_App/Views/frmPokemonForms.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Pokemon_Training_App.Views
+{
+    partial class frmPokemonForms
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmPokemonForms
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 298);
+            this.Name = "frmPokemonForms";
+            this.Text = "Forms | #[###] [POKEMONNAME]";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
