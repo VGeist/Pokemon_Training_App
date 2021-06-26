eb73dbcb5ae377a11b3fb9e7d4181e2b88bedbd5
Victor Geist <victor75664@cccneb.edu>
2021-06-26 09:12:51 -0500

    - Added frmEvolve

---
+++ Pokemon_Training_App/Views/frmEvolve.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Pokemon_Training_App.Views
+{
+    partial class frmEvolve
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnEvolve = new System.Windows.Forms.Button();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.slblNumber = new System.Windows.Forms.Label();
+            this.txtHealthStat = new System.Windows.Forms.TextBox();
+            this.slblHealthStat = new System.Windows.Forms.Label();
+            this.txtSpeedStat = new System.Windows.Forms.TextBox();
+            this.txtSpDefenseStat = new System.Windows.Forms.TextBox();
+            this.txtSpAttackStat = new System.Windows.Forms.TextBox();
+            this.txtDefenseStat = new System.Windows.Forms.TextBox();
+            this.txtAttackStat = new System.Windows.Forms.TextBox();
+            this.slblSpeedStat = new System.Windows.Forms.Label();
+            this.slblSpDefenseStat = new System.Windows.Forms.Label();
+            this.slblSpAttackStat = new System.Windows.Forms.Label();
+            this.slblDefenseStat = new System.Windows.Forms.Label();
+            this.slblAttackStat = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.AutoSize = true;
+            this.btnCancel.Location = new System.Drawing.Point(138, 247);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(85, 32);
+            this.btnCancel.TabIndex = 8;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnEvolve
+            // 
+            this.btnEvolve.AutoSize = true;
+            this.btnEvolve.Location = new System.Drawing.Point(33, 247);
+            this.btnEvolve.Name = "btnEvolve";
+            this.btnEvolve.Size = new System.Drawing.Size(87, 32);
+            this.btnEvolve.TabIndex = 7;
+            this.btnEvolve.Text = "Evolve";
+            this.btnEvolve.UseVisualStyleBackColor = true;
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(145, 7);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(100, 30);
+            this.txtNumber.TabIndex = 0;
+            // 
+            // slblNumber
+            // 
+            this.slblNumber.AutoSize = true;
+            this.slblNumber.Location = new System.Drawing.Point(11, 10);
+            this.slblNumber.Name = "slblNumber";
+            this.slblNumber.Size = new System.Drawing.Size(103, 22);
+            this.slblNumber.TabIndex = 42;
+            this.slblNumber.Text = "Pokemon #";
+            // 
+            // txtHealthStat
+            // 
+            this.txtHealthStat.Location = new System.Drawing.Point(145, 40);
+            this.txtHealthStat.Name = "txtHealthStat";
+            this.txtHealthStat.Size = new System.Drawing.Size(100, 30);
+            this.txtHealthStat.TabIndex = 1;
+            // 
+            // slblHealthStat
+            // 
+            this.slblHealthStat.AutoSize = true;
+            this.slblHealthStat.Location = new System.Drawing.Point(11, 43);
+            this.slblHealthStat.Name = "slblHealthStat";
+            this.slblHealthStat.Size = new System.Drawing.Size(62, 22);
+            this.slblHealthStat.TabIndex = 41;
+            this.slblHealthStat.Text = "Health";
+            // 
+            // txtSpeedStat
+            // 
+            this.txtSpeedStat.Location = new System.Drawing.Point(145, 205);
+            this.txtSpeedStat.Name = "txtSpeedStat";
+            this.txtSpeedStat.Size = new System.Drawing.Size(100, 30);
+            this.txtSpeedStat.TabIndex = 6;
+            // 
+            // txtSpDefenseStat
+            // 
+            this.txtSpDefenseStat.Location = new System.Drawing.Point(145, 172);
+            this.txtSpDefenseStat.Name = "txtSpDefenseStat";
+            this.txtSpDefenseStat.Size = new System.Drawing.Size(100, 30);
+            this.txtSpDefenseStat.TabIndex = 5;
+            // 
+            // txtSpAttackStat
+            // 
+            this.txtSpAttackStat.Location = new System.Drawing.Point(145, 139);
+            this.txtSpAttackStat.Name = "txtSpAttackStat";
+            this.txtSpAttackStat.Size = new System.Drawing.Size(100, 30);
+            this.txtSpAttackStat.TabIndex = 4;
+            // 
+            // txtDefenseStat
+            // 
+            this.txtDefenseStat.Location = new System.Drawing.Point(145, 106);
+            this.txtDefenseStat.Name = "txtDefenseStat";
+            this.txtDefenseStat.Size = new System.Drawing.Size(100, 30);
+            this.txtDefenseStat.TabIndex = 3;
+            // 
+            // txtAttackStat
+            // 
+            this.txtAttackStat.Location = new System.Drawing.Point(145, 73);
+            this.txtAttackStat.Name = "txtAttackStat";
+            this.txtAttackStat.Size = new System.Drawing.Size(100, 30);
+            this.txtAttackStat.TabIndex = 2;
+            // 
+            // slblSpeedStat
+            // 
+            this.slblSpeedStat.AutoSize = true;
+            this.slblSpeedStat.Location = new System.Drawing.Point(11, 208);
+            this.slblSpeedStat.Name = "slblSpeedStat";
+            this.slblSpeedStat.Size = new System.Drawing.Size(59, 22);
+            this.slblSpeedStat.TabIndex = 40;
+            this.slblSpeedStat.Text = "Speed";
+            // 
+            // slblSpDefenseStat
+            // 
+            this.slblSpDefenseStat.AutoSize = true;
+            this.slblSpDefenseStat.Location = new System.Drawing.Point(11, 175);
+            this.slblSpDefenseStat.Name = "slblSpDefenseStat";
+            this.slblSpDefenseStat.Size = new System.Drawing.Size(95, 22);
+            this.slblSpDefenseStat.TabIndex = 39;
+            this.slblSpDefenseStat.Text = "SpDefense";
+            // 
+            // slblSpAttackStat
+            // 
+            this.slblSpAttackStat.AutoSize = true;
+            this.slblSpAttackStat.Location = new System.Drawing.Point(11, 142);
+            this.slblSpAttackStat.Name = "slblSpAttackStat";
+            this.slblSpAttackStat.Size = new System.Drawing.Size(81, 22);
+            this.slblSpAttackStat.TabIndex = 38;
+            this.slblSpAttackStat.Text = "SpAttack";
+            // 
+            // slblDefenseStat
+            // 
+            this.slblDefenseStat.AutoSize = true;
+            this.slblDefenseStat.Location = new System.Drawing.Point(11, 109);
+            this.slblDefenseStat.Name = "slblDefenseStat";
+            this.slblDefenseStat.Size = new System.Drawing.Size(75, 22);
+            this.slblDefenseStat.TabIndex = 37;
+            this.slblDefenseStat.Text = "Defense";
+            // 
+            // slblAttackStat
+            // 
+            this.slblAttackStat.AutoSize = true;
+            this.slblAttackStat.Location = new System.Drawing.Point(11, 76);
+            this.slblAttackStat.Name = "slblAttackStat";
+            this.slblAttackStat.Size = new System.Drawing.Size(61, 22);
+            this.slblAttackStat.TabIndex = 36;
+            this.slblAttackStat.Text = "Attack";
+            // 
+            // frmEvolve
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(257, 286);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnEvolve);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.slblNumber);
+            this.Controls.Add(this.txtHealthStat);
+            this.Controls.Add(this.slblHealthStat);
+            this.Controls.Add(this.txtSpeedStat);
+            this.Controls.Add(this.txtSpDefenseStat);
+            this.Controls.Add(this.txtSpAttackStat);
+            this.Controls.Add(this.txtDefenseStat);
+            this.Controls.Add(this.txtAttackStat);
+            this.Controls.Add(this.slblSpeedStat);
+            this.Controls.Add(this.slblSpDefenseStat);
+            this.Controls.Add(this.slblSpAttackStat);
+            this.Controls.Add(this.slblDefenseStat);
+            this.Controls.Add(this.slblAttackStat);
+            this.Font = new System.Drawing.Font("Cambria", 14.25F);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "frmEvolve";
+            this.Text = "frmEvolve";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnEvolve;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Label slblNumber;
+        private System.Windows.Forms.TextBox txtHealthStat;
+        private System.Windows.Forms.Label slblHealthStat;
+        private System.Windows.Forms.TextBox txtSpeedStat;
+        private System.Windows.Forms.TextBox txtSpDefenseStat;
+        private System.Windows.Forms.TextBox txtSpAttackStat;
+        private System.Windows.Forms.TextBox txtDefenseStat;
+        private System.Windows.Forms.TextBox txtAttackStat;
+        private System.Windows.Forms.Label slblSpeedStat;
+        private System.Windows.Forms.Label slblSpDefenseStat;
+        private System.Windows.Forms.Label slblSpAttackStat;
+        private System.Windows.Forms.Label slblDefenseStat;
+        private System.Windows.Forms.Label slblAttackStat;
+    }
+}
---
+++ Pokemon_Training_App/Views/frmEvolve.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pokemon_Training_App.Views
+{
+    public partial class frmEvolve : Form
+    {
+        public frmEvolve()
+        {
+            InitializeComponent();
+        }
+    }
+}
