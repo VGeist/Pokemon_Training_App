c616c183a17a5177e399fce379bfd0279dd57c1c
Victor Geist <victor75664@cccneb.edu>
2021-06-26 09:10:49 -0500

    - Added frmLevelUp

---
+++ Pokemon_Training_App/Views/frmLevelUp.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Pokemon_Training_App.Views
+{
+    partial class frmLevelUp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtHealthStat = new System.Windows.Forms.TextBox();
+            this.slblHealthStat = new System.Windows.Forms.Label();
+            this.txtSpeedStat = new System.Windows.Forms.TextBox();
+            this.txtSpDefenseStat = new System.Windows.Forms.TextBox();
+            this.txtSpAttackStat = new System.Windows.Forms.TextBox();
+            this.txtDefenseStat = new System.Windows.Forms.TextBox();
+            this.txtAttackStat = new System.Windows.Forms.TextBox();
+            this.slblSpeedStat = new System.Windows.Forms.Label();
+            this.slblSpDefenseStat = new System.Windows.Forms.Label();
+            this.slblSpAttackStat = new System.Windows.Forms.Label();
+            this.slblDefenseStat = new System.Windows.Forms.Label();
+            this.slblAttackStat = new System.Windows.Forms.Label();
+            this.txtLevel = new System.Windows.Forms.TextBox();
+            this.slblLevel = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtHealthStat
+            // 
+            this.txtHealthStat.Location = new System.Drawing.Point(145, 39);
+            this.txtHealthStat.Name = "txtHealthStat";
+            this.txtHealthStat.Size = new System.Drawing.Size(100, 30);
+            this.txtHealthStat.TabIndex = 1;
+            // 
+            // slblHealthStat
+            // 
+            this.slblHealthStat.AutoSize = true;
+            this.slblHealthStat.Location = new System.Drawing.Point(11, 42);
+            this.slblHealthStat.Name = "slblHealthStat";
+            this.slblHealthStat.Size = new System.Drawing.Size(62, 22);
+            this.slblHealthStat.TabIndex = 21;
+            this.slblHealthStat.Text = "Health";
+            // 
+            // txtSpeedStat
+            // 
+            this.txtSpeedStat.Location = new System.Drawing.Point(145, 204);
+            this.txtSpeedStat.Name = "txtSpeedStat";
+            this.txtSpeedStat.Size = new System.Drawing.Size(100, 30);
+            this.txtSpeedStat.TabIndex = 6;
+            // 
+            // txtSpDefenseStat
+            // 
+            this.txtSpDefenseStat.Location = new System.Drawing.Point(145, 171);
+            this.txtSpDefenseStat.Name = "txtSpDefenseStat";
+            this.txtSpDefenseStat.Size = new System.Drawing.Size(100, 30);
+            this.txtSpDefenseStat.TabIndex = 5;
+            // 
+            // txtSpAttackStat
+            // 
+            this.txtSpAttackStat.Location = new System.Drawing.Point(145, 138);
+            this.txtSpAttackStat.Name = "txtSpAttackStat";
+            this.txtSpAttackStat.Size = new System.Drawing.Size(100, 30);
+            this.txtSpAttackStat.TabIndex = 4;
+            // 
+            // txtDefenseStat
+            // 
+            this.txtDefenseStat.Location = new System.Drawing.Point(145, 105);
+            this.txtDefenseStat.Name = "txtDefenseStat";
+            this.txtDefenseStat.Size = new System.Drawing.Size(100, 30);
+            this.txtDefenseStat.TabIndex = 3;
+            // 
+            // txtAttackStat
+            // 
+            this.txtAttackStat.Location = new System.Drawing.Point(145, 72);
+            this.txtAttackStat.Name = "txtAttackStat";
+            this.txtAttackStat.Size = new System.Drawing.Size(100, 30);
+            this.txtAttackStat.TabIndex = 2;
+            // 
+            // slblSpeedStat
+            // 
+            this.slblSpeedStat.AutoSize = true;
+            this.slblSpeedStat.Location = new System.Drawing.Point(11, 207);
+            this.slblSpeedStat.Name = "slblSpeedStat";
+            this.slblSpeedStat.Size = new System.Drawing.Size(59, 22);
+            this.slblSpeedStat.TabIndex = 17;
+            this.slblSpeedStat.Text = "Speed";
+            // 
+            // slblSpDefenseStat
+            // 
+            this.slblSpDefenseStat.AutoSize = true;
+            this.slblSpDefenseStat.Location = new System.Drawing.Point(11, 174);
+            this.slblSpDefenseStat.Name = "slblSpDefenseStat";
+            this.slblSpDefenseStat.Size = new System.Drawing.Size(95, 22);
+            this.slblSpDefenseStat.TabIndex = 16;
+            this.slblSpDefenseStat.Text = "SpDefense";
+            // 
+            // slblSpAttackStat
+            // 
+            this.slblSpAttackStat.AutoSize = true;
+            this.slblSpAttackStat.Location = new System.Drawing.Point(11, 141);
+            this.slblSpAttackStat.Name = "slblSpAttackStat";
+            this.slblSpAttackStat.Size = new System.Drawing.Size(81, 22);
+            this.slblSpAttackStat.TabIndex = 15;
+            this.slblSpAttackStat.Text = "SpAttack";
+            // 
+            // slblDefenseStat
+            // 
+            this.slblDefenseStat.AutoSize = true;
+            this.slblDefenseStat.Location = new System.Drawing.Point(11, 108);
+            this.slblDefenseStat.Name = "slblDefenseStat";
+            this.slblDefenseStat.Size = new System.Drawing.Size(75, 22);
+            this.slblDefenseStat.TabIndex = 14;
+            this.slblDefenseStat.Text = "Defense";
+            // 
+            // slblAttackStat
+            // 
+            this.slblAttackStat.AutoSize = true;
+            this.slblAttackStat.Location = new System.Drawing.Point(11, 75);
+            this.slblAttackStat.Name = "slblAttackStat";
+            this.slblAttackStat.Size = new System.Drawing.Size(61, 22);
+            this.slblAttackStat.TabIndex = 13;
+            this.slblAttackStat.Text = "Attack";
+            // 
+            // txtLevel
+            // 
+            this.txtLevel.Location = new System.Drawing.Point(145, 6);
+            this.txtLevel.Name = "txtLevel";
+            this.txtLevel.Size = new System.Drawing.Size(100, 30);
+            this.txtLevel.TabIndex = 0;
+            // 
+            // slblLevel
+            // 
+            this.slblLevel.AutoSize = true;
+            this.slblLevel.Location = new System.Drawing.Point(11, 9);
+            this.slblLevel.Name = "slblLevel";
+            this.slblLevel.Size = new System.Drawing.Size(53, 22);
+            this.slblLevel.TabIndex = 26;
+            this.slblLevel.Text = "Level";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.AutoSize = true;
+            this.btnCancel.Location = new System.Drawing.Point(138, 246);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(85, 32);
+            this.btnCancel.TabIndex = 8;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.AutoSize = true;
+            this.btnConfirm.Location = new System.Drawing.Point(33, 246);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(87, 32);
+            this.btnConfirm.TabIndex = 7;
+            this.btnConfirm.Text = "Confirm";
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            // 
+            // frmLevelUp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(257, 286);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnConfirm);
+            this.Controls.Add(this.txtLevel);
+            this.Controls.Add(this.slblLevel);
+            this.Controls.Add(this.txtHealthStat);
+            this.Controls.Add(this.slblHealthStat);
+            this.Controls.Add(this.txtSpeedStat);
+            this.Controls.Add(this.txtSpDefenseStat);
+            this.Controls.Add(this.txtSpAttackStat);
+            this.Controls.Add(this.txtDefenseStat);
+            this.Controls.Add(this.txtAttackStat);
+            this.Controls.Add(this.slblSpeedStat);
+            this.Controls.Add(this.slblSpDefenseStat);
+            this.Controls.Add(this.slblSpAttackStat);
+            this.Controls.Add(this.slblDefenseStat);
+            this.Controls.Add(this.slblAttackStat);
+            this.Font = new System.Drawing.Font("Cambria", 14.25F);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "frmLevelUp";
+            this.Text = "Level Up";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtHealthStat;
+        private System.Windows.Forms.Label slblHealthStat;
+        private System.Windows.Forms.TextBox txtSpeedStat;
+        private System.Windows.Forms.TextBox txtSpDefenseStat;
+        private System.Windows.Forms.TextBox txtSpAttackStat;
+        private System.Windows.Forms.TextBox txtDefenseStat;
+        private System.Windows.Forms.TextBox txtAttackStat;
+        private System.Windows.Forms.Label slblSpeedStat;
+        private System.Windows.Forms.Label slblSpDefenseStat;
+        private System.Windows.Forms.Label slblSpAttackStat;
+        private System.Windows.Forms.Label slblDefenseStat;
+        private System.Windows.Forms.Label slblAttackStat;
+        private System.Windows.Forms.TextBox txtLevel;
+        private System.Windows.Forms.Label slblLevel;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnConfirm;
+    }
+}
